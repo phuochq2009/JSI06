53d932e567626ece574683e9056fae14e52534dc
phuochq2009 <phuochq2009@gmail.com>
2025-06-22 09:56:21 +0700

    add checklogin in orders page

--- js/orders.js
+++ js/orders.js
@@ -60,3 +60,62 @@ function displayOrders() {
             ordersContainer.innerHTML = "<p>Error fetching orders.</p>";
         });
 }
+
+function checkLoginStatus() {
+  const textt = document.getElementById("textt");
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      db.collection("users")
+        .where("email", "==", user.email)
+        .get()
+        .then((querySnapshot) => {
+          console.log("Query Snapshot:", querySnapshot);
+
+          if (user.email === "admin2k9@gmail.com") {
+            const userData = querySnapshot.docs[0].data();
+
+            textt.innerHTML = `
+                        <div class="dropdown">
+                            <p class="nav-link">
+                                ${userData.username} <i class="fa-solid fa-caret-down">
+                                    <button class="drop-content" onclick="logout()">Log Out</button>
+                                </i>
+                            </p>
+                        </div>
+                    `;
+          } else {
+            location.href = "index.html";
+          }
+          console.log("User is signed in:", user.email);
+        })
+        .catch((error) => {
+          console.log("Error getting documents: ", error);
+          textt.innerHTML = `<a class="nav-link" href="login.html">Log in</a>`;
+        });
+    }  else {
+      textt.innerHTML = `<a class="nav-link" href="login.html">Log in</a>`;
+      console.log("No user is signed in.");
+      location.href = "login.html";
+    }
+  });
+}
+
+
+
+checkLoginStatus();
+
+function logout() {
+  firebase
+    .auth()
+    .signOut()
+    .then(() => {
+      // Sign-out successful.
+      console.log("Sign-out successful.");
+      alert("You have signed out successfully!");
+    })
+    .catch((error) => {
+      // An error happened.
+      console.log("An error happened:", error);
+      alert("Error during sign out: " + error.message);
+    });
+}
